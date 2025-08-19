b35240aaf3f3e27b1f29d23e1f6a67e11cd7987a
LucasChambi <lucasbrunocc880@outlook.com>
2025-08-18 22:08:44 -0300

    Continuação com Laravel e Introdução ao Tailwindcss

--- aula02/routes/web.php
+++ aula02/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return view('teste');
+});
+
+/* Route::get('/sua-rota', function () {
+    return view('arquivo.blade.php');
+});
+*/
