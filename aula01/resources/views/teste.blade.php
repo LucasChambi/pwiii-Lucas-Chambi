a09ab9d940f7cb1416402975405ae8b8c2f7e82f
LucasChambi <lucasbrunocc880@outlook.com>
2025-09-20 13:17:05 -0300

    Validação de usuário

--- aula01/resources/views/teste.blade.php
+++ aula01/resources/views/teste.blade.php
@@ -1,3 +1,3 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+@php
+    echo "Bem-vindo à página de teste!";
+@endphp
--- aula01/routes/web.php
+++ aula01/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rota para a view teste.blade.php
+Route::get('/teste', function () {
+    return view('teste');
+});
