68a0b6858b34f0ea1ade8151cec9944c8a5b8457
LucasChambi <lucasbrunocc880@outlook.com>
2025-12-10 23:56:36 -0300

    Fazendo edições

--- aula01/resources/views/teste.blade.php
+++ aula01/resources/views/teste.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Teste Concluído</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <h1>Bem-vindo à página de teste!</h1>
-    <p>Esta é uma página simples para testar a rota /teste.</p>
-</body>
-</html>
+<body class="bg-amber-200 text-center p-10">
+    <h1 class="text-2xl">Bem-vindo à página de teste!</h1>
+    <p class="text-4xl font-bold">Olá, mundo</p>
+    </body>
+    </html>
--- aula01/resources/views/welcome.blade.php
+++ aula01/resources/views/welcome.blade.php
@@ -1,21 +1,10 @@
-{{--<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verificação de Senha</title>
 </head>
-<body>
-    <h1>Verificar senha</h1>
-    @csrf
-    <form action="" method="GET">
-        Email: <input type="email" name="email" required><br>
-        <br>
-        Senha: <input type="text" name="senha" required><br>
-        <br>
-        <input type="submit" name="submit" value="testar"><br>
-        <br>
-    </form>
     {{--GET - irá coletar dados--}}
     {{--POST - irá enviar dados--}}
     {{--PUT - irá atualizar dados--}}
@@ -26,23 +15,6 @@
     {{--OPTIONS - irá coletar as opções da requisição--}}
     {{--CONNECT - irá conectar a um servidor--}}
     {{--TRACE - irá rastrear a requisição--}}
-    {{--Os credenciais corretos vai direcionar para a pagina de teste com a tag script e o metodo
-        window.location.href(utilizado para redirecionar o usuario para outra pagina)--}}
-    {{--@php
-    if(isset($_GET['senha']) && isset($_GET['email'])){
-        $senha = $_GET['senha'];
-        $email = $_GET['email'];
-        if($senha == "12345" && $email == "admin@email.com"){
-            echo "<script>
-                  window.location.href = '".url('/teste')."';
-                  </script>";
-        } else {
-            echo "Senha ou email incorretos!";
-        }
-    }
-    @endphp
-</body>
-</html>--}}
 <form method="POST" action="{{ url('teste') }}">
     @csrf
     {{-- Campos do formulário --}}
